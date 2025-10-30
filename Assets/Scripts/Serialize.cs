9e4d49ef7657f117290438a211cc48fc6e91d5ed
Joan Marquès Besses <85459149+joanmarquesbesses@users.noreply.github.com>
2025-10-30 14:46:04 +0100

    trying serialize

--- Assets/Scripts/Serialize.cs
+++ Assets/Scripts/Serialize.cs
@@ -1,36 +1,40 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
+public class Pokemon
+{
+    public string name;
+
+    public Pokemon(string name)
+    {
+        this.name = name;
+    }
+}
+
+public struct DTO
+{
+    public string playerName;
+    public int level;
+    public List<Pokemon> ownedPokemons;
+
+    public DTO(string player, int lvl, List<Pokemon> owned)
+    {
+        playerName = player;
+        level = lvl;
+        ownedPokemons = owned;
+    }
+}
+
+
+
 public class Serialize : MonoBehaviour
 {
-    public class Pokemon
-    {
-        public string name;
-
-        public Pokemon(string name)
-        {
-            this.name = name;
-        }
-    }
-
-    public struct DTO
-    {
-        string playerName;
-        int level;
-        List<Pokemon> ownedPokemons;
-
-        public DTO(string player, int lvl, List<Pokemon> owned)
-        {
-            playerName = player;
-            level = lvl;
-            ownedPokemons = owned;
-        }
-    }
 
     MemoryStream stream;
 
-    void Start()
+    public void Start()
     {
         Pokemon charmander = new Pokemon("Charmander");
         Pokemon bulbasur = new Pokemon("Bulbasur");
@@ -40,7 +44,15 @@ public class Serialize : MonoBehaviour
 
         DTO data = new DTO("Joan", 10, list);
 
+        Debug.Log($"{data.playerName}");
+        Debug.Log($"{data.level}");
+        for (int i = 0; i < data.ownedPokemons.Count; i++)
+        {
+            Debug.Log($"{data.ownedPokemons[i].name}");
+        }
+
         SerializeJson(data);
+        Deserialize();
     }
 
 
@@ -50,10 +62,24 @@ public class Serialize : MonoBehaviour
         stream = new MemoryStream();
         BinaryWriter writer = new BinaryWriter(stream);
         writer.Write(json);
+        List<Pokemon> list = new List<Pokemon>(data.ownedPokemons);
+        writer.Close();
+
     }
 
     void Deserialize()
     {
-
+        DTO t;
+        BinaryReader reader = new BinaryReader(stream);
+        stream.Seek(0, SeekOrigin.Begin);
+        string json = reader.ReadString();
+        t = JsonUtility.FromJson<DTO>(json);
+        Debug.Log(t.playerName);
+        Debug.Log(t.level);
+        List<Pokemon> list = new List<Pokemon>(t.ownedPokemons);
+        for (int i = 0; i < list.Count; i++)
+        {
+            Debug.Log($"{list[i].name}");
+        }
     }
 }
